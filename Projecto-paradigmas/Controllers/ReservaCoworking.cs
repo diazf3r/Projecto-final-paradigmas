3c6dab98df0ba550942cd17138ec7029ee62aab3
Omar F Diaz <omarpadilla1472@gmail.com>
2026-07-28 18:55:52 -0600

    Add project files.

---
+++ Projecto-paradigmas/Controllers/ReservaCoworking.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Projecto_paradigmas.Controllers
+{
+    public class ReservaCoworking : Controller
+    {
+        // GET: ReservaCoworking
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ReservaCoworking/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ReservaCoworking/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ReservaCoworking/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ReservaCoworking/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ReservaCoworking/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ReservaCoworking/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ReservaCoworking/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Projecto-paradigmas/Models/coworkingReservations.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projecto_paradigmas.Models
+{
+    public class coworkingReservations
+    {
+        public int ReservationId { get; set; }
+
+        public int AreaId { get; set; }
+
+        public int Participants { get; set; }
+
+        public int ReservedBy { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime End { get; set; }
+        public ReservationStatus Status { get; set; } = ReservationStatus.Pending;
+        public bool IsCheckedIn { get; set; } = false;
+
+        public DateTime? CheckInTime { get; set; }
+
+        [StringLength(200)]
+        public string? AcademicPurpose { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? CancelledAt { get; set; }
+        public string? CancellationReason { get; set; }
+    }
+
+    public enum ReservationStatus
+    {
+        Pending = 1,
+        Confirmed = 2,
+        Completed = 3,
+        Cancelled = 4,
+        NoShow = 5
+    }
+}
